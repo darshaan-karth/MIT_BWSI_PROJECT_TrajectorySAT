23d792872ffa03346ecab732d40cac7ab1091050
gk-karthick2k <64374614+gk-karthick2k@users.noreply.github.com>
2025-01-04 11:40:37 -0600

    Cloning repo for raspberrypi 4b access

--- main_code.py
+++ main_code.py
@@ -10,15 +10,12 @@ The provided functions are only for reference, you do not need to use them.
 You will need to complete the take_photo() function and configure the VARIABLES section
 """
 
-#AUTHOR: 
-#DATE:
-
 #import libraries
 import time
 import board
 from adafruit_lsm6ds.lsm6dsox import LSM6DSOX as LSM6DS
 from adafruit_lis3mdl import LIS3MDL
-#from git import Repo
+from git import Repo
 from picamera2 import Picamera2
 
 #VARIABLES
